5249c42c1feb7beb52c6c3f19ad230fdb985948b
Leandro23112007 <146172637+Leandro23112007@users.noreply.github.com>
2025-01-15 09:13:37 +0000

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -10,8 +10,13 @@ class Game {
         this.player = new Player(this);
         this.obstacles = [];
         this.numberOfObstacles = 10;
+        this.ctx.textAlign = 'right';
+        this.ctx.font = '15px Bungee';
         this.gravity;
         this.speed;
+        this.score;
+        this.gameOver;
+        this.timer;
 
         this.resize(window.innerWidth, window.innerHeight);
 
@@ -19,19 +24,16 @@ class Game {
             this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
         });
 
-        // Mouse controls
         this.canvas.addEventListener('mousedown', e => {
             this.player.flap();
         });
 
-        // Keyboard controls
         window.addEventListener('keydown', e => {
             if((e.key === ' ') || (e.key === 'Enter'))  {
                 this.player.flap();
             }
         });
 
-        // Touch controls
         this.canvas.addEventListener('touchstart', e => {
             this.player.flap();
         });
@@ -43,6 +45,7 @@ class Game {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.ctx.fillStyle = '#5995F2';
+        this.ctx.font = '15px Bungee';
         this.ratio = this.height / this.baseHeight;
 
         this.gravity = 0.15 * this.ratio;
@@ -53,12 +56,16 @@ class Game {
         this.obstacles.forEach(obstacle => {
             obstacle.resize();
         });
+        this.score = 0;
+        this.gameOver = false;
+        this.timer = 0;
     }
-
+    
     render() {
         this.background.update();
         this.background.draw();
         this.player.update();
+        this.drawStatusText();
         this.player.draw();
         this.obstacles.forEach(obstacle => {
             obstacle.update();
@@ -75,6 +82,15 @@ class Game {
             this.obstacles.push(new Obstacle(this, firstX + i * obstacleSpacing));
         }
     }
+    drawStatusText() {
+        this.ctx.save();
+        this.ctx.fillStyle = '#FFFFFF';
+        this.ctx.textAlign = 'right'
+        this.ctx.fillText("Score: " + this.score, this.width - 10 ,30);
+        this.ctx.textAlign = 'left'
+        this.ctx.fillText("Time: " + this.timer, 10, 30);
+        this.ctx.restore();
+    }
 }
 
 window.addEventListener('load', function() {
@@ -84,6 +100,16 @@ window.addEventListener('load', function() {
     canvas.height = 720;
 
     const game = new Game(canvas, ctx);
+
+    let lastTime = 0;
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        console.log(lastTime);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.render(deltaTime);
+        if(!game.gameOver) requestAnimationFrame(animate);
+    }
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
--- js/obstacle.js
+++ js/obstacle.js
@@ -21,7 +21,9 @@ class Obstacle  {
         if(this.isOffScreen())  {
             this.markedForDeletion = true;
             this.game.obstacles = this.game.obstacles.filter(obstacle => !obstacle.markedForDeletion);
-            console.log(this.game.obstacles.length);
+            (this.game.obstacles.length) 
+            if (this.game.obstacles.leght <= 0) this.game.gameOver = true;
+                this.game.score++;
         }
     }
 
@@ -35,6 +37,6 @@ class Obstacle  {
     }
 
     isOffScreen()   {
-        return this.x < 0;
+        return this.x < -this.scaledWidth;
     }
 }
